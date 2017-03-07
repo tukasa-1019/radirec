6b8f4e52e7c64099ebfc3068c745c57ee9519d47
tukasa_1019 <tukasa.1019@gmail.com>
2017-03-07 11:29:13 +0900

    [Chg]: title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>rtmpdump</title>
+  <title>RadiRec</title>
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <script src="js/jquery-1.12.4.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
